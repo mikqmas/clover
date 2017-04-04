66f95d546adf02e961fc5f1530489fb93d70c3d0
Samuel Kim <samuel.kim@clover.com>
2017-04-04 16:36:55 -0700

    delete token and readme

--- deleteAllOpenOrders.rb
+++ deleteAllOpenOrders.rb
@@ -2,11 +2,12 @@ require "net/http"
 require "uri"
 require 'json'
 
-mid = "EJE2ZH35JJAG2"
-employee = "RKGA7MJ6927WC"
-auth_token = "e462d53f-0ef5-c500-65e9-350e7129ab73"
+env = "sandbox.dev.clover.com" # or api.clover.com for production
+mid = #merchant id
+employee = #employee id
+auth_token = #oauth or merchant token
 
-uri = URI.parse("https://sandbox.dev.clover.com/v3/merchants/#{mid}/employees/#{employee}/orders?filter=state=open&limit=1000")
+uri = URI.parse("https://#{env}/v3/merchants/#{mid}/employees/#{employee}/orders?filter=state=open&limit=1000")
 
 request = Net::HTTP::Get.new(uri.request_uri)
 request.add_field 'Authorization', 'Bearer ' + auth_token
@@ -20,7 +21,7 @@ json = JSON.parse(response.body)
 
 json["elements"].each do |el|
   id = el["id"]
-  uri = URI.parse("https://sandbox.dev.clover.com/v3/merchants/#{mid}/orders/#{id}")
+  uri = URI.parse("https://#{env}/v3/merchants/#{mid}/orders/#{id}")
   request = Net::HTTP::Delete.new(uri.request_uri)
   request.add_field 'Authorization', 'Bearer ' + auth_token
   http = Net::HTTP.new(uri.host, uri.port)
---
+++ readme.md
@@ -0,0 +1,2 @@
+In order to clockout, all orders associated with an employee must be deleted. This ruby script deletes all open order for an employee.
+https://github.com/mikqmas/clover/blob/master/deleteAllOpenOrders.rb
